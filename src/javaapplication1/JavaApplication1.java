6a3871cc0d1ad660b57df1b2c4d9a758cf82c790
Gabriel Fuentes Villasevil <g.fuentes.2018@alumnos.urjc.es>
2020-03-06 18:38:05 +0100

    esto es un comentario

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author g.fuentes.2018
+ */
+public class JavaApplication1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        
+      /**  byte miBite =4;
+        short x = 34;
+        int Dirección = 2000000000;
+        float pi = 3.14f;
+        double n = 3.14159265359;
+        boolean b = true;
+        char mi_caracter= 's';
+        
+        Dirección = (byte) miBite;
+        
+        int lio = x++;
+        System.out.println(lio);
+        System.out.println(x);
+        */
+       
+       
+    }
+    
+}
+
